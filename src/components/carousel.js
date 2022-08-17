58155fe1bba428d78fd27e88b80a87f575ef0cb1
Marissa Angell <maa.angell@gmail.com>
2022-08-16 19:30:27 -0600

    Fix left/right arrow icon sizing for imgCarousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -30,14 +30,18 @@ const ImgCarousel = ({ images }) => {
 const [currentIndex, setCurrentIndex] = useState(0)
 
 return (
-    <div className="max-w-sm m-auto">
+    <div className="m-auto p-2">
       <div className="w-full relative select-none">
-        <img src={images[currentIndex]} alt="" className="mx-auto "/>
-        <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-3">
-            <button onClick={handleOnPrevClick}><ChevronLeftIcon/></button>
-            <button onClick={handleOnNextClick}><ChevronRightIcon/></button>
+      	<div className="w-5/6 mx-auto aspect-video block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"
+      		 style={{ backgroundImage: `url(${images[currentIndex] || ''})` }}
+  		 >
+        	<img src={images[currentIndex]} alt="" className="mx-auto hidden"/>
         </div>
-        <div className="w-full flex flex-row justify-center gap-2 mx-auto">
+        <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-3">
+            <button onClick={handleOnPrevClick} ><ChevronLeftIcon className="w-6 h-6 md:w-8 md:h-8"/></button>
+            <button onClick={handleOnNextClick}><ChevronRightIcon className="w-6 h-6 md:w-8 md:h-8"/></button>
+        </div>
+        <div className="w-full flex flex-row justify-center gap-2 mx-auto mt-2 ">
           { images.map((image, index) => (
             index === currentIndex
             ? <button className="bg-gray-400 rounded-full p-2" />
